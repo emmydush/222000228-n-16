cea76ffe3b42641e78f57f0d69c7c605c45d47df
Dushimirimana emmanuel <103512724+emmydush@users.noreply.github.com>
2023-11-04 15:48:59 +0200

    Update brea.java

--- new java/brea.java
+++ new java/brea.java
@@ -1,13 +1,13 @@
 public class brea {
     public static void main(String[] args) {
-        int count = 2;
+        int count = 2;//initialize variale.
 
-        while (true) {
-            if (count > 6) {
+        while (true) {//start an ifinite loop
+            if (count > 6) {// cheque if count is greather than 6
                 break;
             }
-            System.out.println("count:" + count);
-            count++;
+            System.out.println("count:" + count);// print he alue of the count to the console
+            count++;//increment of the value by 1.
         }
     }
 }
