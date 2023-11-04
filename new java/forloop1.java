78530686afcf0a0b1834bab3e108ea0d7c9c6cc2
Dushimirimana emmanuel <103512724+emmydush@users.noreply.github.com>
2023-11-04 16:06:09 +0200

    Update forloop1.java

--- new java/forloop1.java
+++ new java/forloop1.java
@@ -1,11 +1,11 @@
 public class forloop1 {
     public static void main(String[] args) {
-        int sum = 0;
+        int sum = 0;// Initialize a variable 'sum' to store the sum of numbers.
 
         for (int i = 1; i <= 10; i++) {
-            sum += i;
+            sum += i;// Add the current value of 'i' to 'sum' in each iteration.
         }
-        System.out.println("sum of numbers from 1 to 10:" + sum);
+        System.out.println("sum of numbers from 1 to 10:" + sum);// Print the sum of numbers from 1 to 10.
     }
 
 }
