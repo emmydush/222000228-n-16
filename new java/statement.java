3de21f935b2ffb073862db432cff0702dce26690
Dushimirimana emmanuel <103512724+emmydush@users.noreply.github.com>
2023-11-04 16:18:47 +0200

    Update statement.java

--- new java/statement.java
+++ new java/statement.java
@@ -1,9 +1,12 @@
+//this program shows the statement program
 public class statement {
     public static void main(String[]args){
+        //declaration of two integer variables a and b.
 int a=5;
 int b = 10;
-
+//check if the variable a is greater than b
 if (a > b) {
+    // If 'a' is not greater than 'b', execute this block of code and print the following message.
     System.out.println("a is greater than b");
 }else{
     System.out.println("b is greater than a");
