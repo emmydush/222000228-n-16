f5ae0d3b79ad4f28a0536d3c438b3ddee289826a
Dushimirimana emmanuel <103512724+emmydush@users.noreply.github.com>
2023-11-04 15:24:41 +0200

    Update emmanuelLoanl.java

--- object.java/emmanuelLoanl.java
+++ object.java/emmanuelLoanl.java
@@ -12,22 +12,26 @@ public class emmanuelLoanl {
     }
 
     public double calculateTotalpayment() {
+        //calculate the monthly interest rate
         double monthlyRate = annualRate / monthsPerYear;
+        //calculate the number of compaundig peiod
         int totalpayments = monthsPerYear * years;
+        //caculate te final amount with compaund interest uing the compaund interest
         double totalpayment = principal * Math.pow(1 + monthlyRate, totalpayments);
         return totalpayment;
     }
 
     public static void main(String[] args) {
 
-        double principal = 500000;
-        double annualRate = 0.18;
-        int monthsperyears = 12;
+        double principal = 500000;//principal amount
+        double annualRate = 0.18;//annual interestrate
+        int monthsperyears = 12;//Loan term n years
         int years = 3;
 
         emmanuelLoanl loanCalculator = new emmanuelLoanl(principal, annualRate, years, monthsperyears);
 
         double totalAmount = loanCalculator.calculateTotalpayment();
+        //Display the result
         System.out.println("Total amount paid after" + years + "years:frw" + totalAmount);
     }
 }
