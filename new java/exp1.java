094aadb8e34e9b9a9b63fb5fa1e50b4e798907ed
Dushimirimana emmanuel <103512724+emmydush@users.noreply.github.com>
2023-11-04 16:23:29 +0200

    Update exp1.java

--- new java/exp1.java
+++ new java/exp1.java
@@ -1,6 +1,7 @@
+// this is the second program that shows statement program
 public class exp1 {
     public static void main(String[]args){
-        int Experience = 5;
+        int Experience = 5;//initialization of variable 5.
 
         if (Experience < 1) {
             System.out.println("You are a hard worker");
@@ -9,7 +10,7 @@ public class exp1 {
         } else if (Experience >= 5 && Experience < 10) {
             System.out.println("You are a senior worker");
         } else {
-            System.out.println("You are a weak worker");
+            System.out.println("You are a weak worker");//display the output.
         }
     }
 }
