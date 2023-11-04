014be6b390f6b4659c02264fb455700dad5a6fa7
Dushimirimana emmanuel <103512724+emmydush@users.noreply.github.com>
2023-11-04 15:34:02 +0200

    Update Dowhileloop4.java

--- new java/Dowhileloop4.java
+++ new java/Dowhileloop4.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class Dowhileloop4 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);//creating scannner object to read the output
 
-        int secretNumber = 12;
-        int guess;
+        int secretNumber = 12;//the secret number to be guessed.
+        int guess;//variable to store the user guess.
         do {
-            System.out.println("guess the number");
-            guess = scanner.nextInt();
+            System.out.println("guess the number");//prompt the user to guess the number.
+            guess = scanner.nextInt();//read the user input and store it in the guess variabe.
         } while (guess != secretNumber);
-        System.out.println("congratulations ! you hava guesed the right number.");
+        System.out.println("congratulations ! you hava guesed the right number.");//print the result.
     }
 }
